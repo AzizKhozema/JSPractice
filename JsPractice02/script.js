b0786c86025aa063618d3214f2939920844102b7
AzizKhozema <azizdon4243@gmail.com>
2025-04-30 16:22:59 +0500

    JsPractice02 Done

---
+++ JsPractice02/script.js
@@ -0,0 +1,11 @@
+/*
+2. Print Current Window Contents
+
+Write a JavaScript program to print the current window contents.  
+*/
+
+function printwin() {
+    window.print(); // syntax to print the current window contents.
+    // window.print() is a method that opens the print dialog for the current window.
+}
+
